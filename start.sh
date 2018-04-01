6f1a8c4c3295f6f63ce9d2c8311829f93a218eb8
Erlan <ebekenov@hungryfishmedia.com>
2018-04-01 11:49:55 -0400

    modify start.sh to use flask_sockets.worker

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./bin/activate
-gunicorn --workers 3 --timeout 120 --worker-class eventlet --access-logfile=access.log --error-logfile=error.log --bind 0.0.0.0:5000 wsgi
+gunicorn -k flask_sockets.worker -b 0.0.0.0:5000 server:app
